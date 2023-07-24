c6e08b1e4b2e8b583bf52fb02d7a099a143b9356
Michael Rose <michael@rosenetwork.net>
2023-07-24 16:29:21 -0700

    now generates bad jokes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var axios = require('axios')
 var router = express.Router();
+async function randomJoke (){
+   return axios.get("https://api.chucknorris.io/jokes/random").then((x)=>x.data.value).catch((e)=>{
+    console.log(e)
+    res.render('error')
+  })
+}
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  res.render('index', { title: 'Get Chucked'});
+})
+router.get('/joke', async function(req, res, next) {
+  res.render('joke', { title: 'Chuck It To Me', joke: await randomJoke() });
 });
 
 module.exports = router;
